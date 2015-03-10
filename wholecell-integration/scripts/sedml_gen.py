ab109d9a63a04ef678f844b42cb02a14288438ae
Bertrand Moreau <bertrand.moreau@thecosmocompany.com>
2015-03-10 17:10:22 +0100

    SED-ML generation
    - created 'scripts' folder
    - created empty 'sedml_gen.py' Python script

---
+++ wholecell-integration/scripts/sedml_gen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import libsbml, libsedml
+
+# Python script for generating the master SED-ML file for the wholecell model
+
+# Required inputs
+model_files = [] # list of SBML files to be scheduled
+io_files = # list of CSV files
